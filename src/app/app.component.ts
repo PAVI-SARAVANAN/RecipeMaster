22a022a0e5bde3a0afa27d7c43e0d839e1901118
PAVI-SARAVANAN <paviithra261@gmail.com>
2024-09-11 13:24:19 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeComponent } from "./recipe/recipe.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RecipeMaster';
+
+  
 }
